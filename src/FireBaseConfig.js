99fb104cf22e775cc2f0603532d2e3902da7ae75
Yumna Mubeen <yumnamubeen0019@gamil.com>
2025-04-13 22:38:02 +0500

    hosting

--- src/FireBaseConfig.js
+++ src/FireBaseConfig.js
@@ -8,18 +8,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-  // apiKey: "AIzaSyCma10i1o1HOXVp9zc_Dl6RAkYmMW41XBA",
-  // authDomain: "iad-project-ce136.firebaseapp.com",
-  // projectId: "iad-project-ce136",
-  // storageBucket: "iad-project-ce136.firebasestorage.app",
-  // messagingSenderId: "140041687071",
-  // appId: "1:140041687071:web:63d10e72819ab9ca70c52a"
+  // apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  // authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCma10i1o1HOXVp9zc_Dl6RAkYmMW41XBA",
+  authDomain: "iad-project-ce136.firebaseapp.com",
+  projectId: "iad-project-ce136",
+  storageBucket: "iad-project-ce136.firebasestorage.app",
+  messagingSenderId: "140041687071",
+  appId: "1:140041687071:web:63d10e72819ab9ca70c52a"
 };
 
 // Initialize Firebase
